29b340fb6a6046dd5bc1c0438e4ce1e105736e01
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-06-06 17:05:36 -0300

    Novidades Servlet API 3.0

--- src/br/com/rafaelvicio/jee/servlet/OiMundo.java
+++ src/br/com/rafaelvicio/jee/servlet/OiMundo.java
@@ -3,6 +3,8 @@ package br.com.rafaelvicio.jee.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//@WebServlet(name="MinhaServlet", urlPatterns={"/oi", "/ola"})
+@WebServlet("/oi")
 public class OiMundo extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletREsponse response) throws ServletException, IOException {
 
